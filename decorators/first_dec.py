6a72498807ab115d775571f60e33dc2018035227
omar115 <omarhasan115@gmail.com>
2021-05-09 13:11:53 +0600

    hi

--- decorators/first_dec.py
+++ decorators/first_dec.py
@@ -1,10 +1,11 @@
+# normal function to illustrate basic activity of function
 def plus_one(val1, val2):
     sum = val1 + val2
     return sum
 
 print(plus_one(7,3))
 
-
+# defining function inside another function
 def plus_two(val1,val2):
     def add(val1):
         sum = val1+10
