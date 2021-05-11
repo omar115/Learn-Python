905140c8d5ffa90a62dc10c0413a622816c954e4
omar115 <omarhasan115@gmail.com>
2021-05-11 17:58:36 +0600

    cmmit

--- multiple_args/test.py
+++ multiple_args/test.py
@@ -1,8 +1,8 @@
 def sum(*args):
-    result = 0
+    result = ''
     for x in args:
-        result += x
+        result += str(x)
     return result
     # return a+b+c+d
 
-print(sum(1,2,3,4))
+print(sum(1,2,3,4, ' Hello? ', 3.14, ' How are you?'))
