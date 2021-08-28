775c0f0bc3f642367b8e3de4073584780d833365
omar115 <omarhasan115@gmail.com>
2021-08-28 17:15:36 +0600

    notifier added

---
+++ automation/notifier.py
@@ -0,0 +1,9 @@
+from win10toast import ToastNotifier
+
+toaster = ToastNotifier()
+
+toaster.show_toast("Hello World!",
+"python in 10 lines",
+# icon_path="C:\Users\Ferntech\Desktop\git_workspace\Learn-Python\automation\sample.ico",
+duration=3
+)
