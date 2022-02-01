60d1f6a6c9b16bfbc9c943d0e17d1a838bd89abb
omar115 <omarhasan115@gmail.com>
2022-02-02 03:08:31 +0600

    removed files

--- functional-programming/lecture-1.py
+++
@@ -1,7 +0,0 @@
-# define list of dictionaries
-scientists = [
-    {"name": "Ada lovelace", "field": "math", "born": 1815, "Noble": False},
-    {"name": "Emmy Noether", "field": "math", "born": 1882, "Noble": False},
-]
-
-print(scientists)
