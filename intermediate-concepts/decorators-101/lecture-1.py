4d3d2bfabdb35bedc04a708c98eeb0322f170e3c
omar115 <omarhasan115@gmail.com>
2022-02-02 03:20:58 +0600

    basic-function-before-decorator

---
+++ intermediate-concepts/decorators-101/lecture-1.py
@@ -0,0 +1,10 @@
+def add_one(number):
+    """
+    A function that adds one to the number given and returns it
+
+    :param index: integer object
+    :return: integer object
+    """
+    return number + 1
+
+print(add_one(10))
